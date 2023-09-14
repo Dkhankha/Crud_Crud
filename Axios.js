b3b0165e292d7ac2e6ee862ce6a6c1467280807b
Dkhankha <140619121+Dkhankha@users.noreply.github.com>
2023-09-14 12:37:35 +0530

    functionality with delete button update

--- Axios.js
+++ Axios.js
@@ -7,16 +7,47 @@ function saveData(event){
         "name":name,
         "email":email
     }  
-   axios.post('https://crudcrud.com/api/4edc25d4d58848728b4b4f20db709230/appointmentData',obj)
+   axios.post('https://crudcrud.com/api/cfabb4d7a35548378df0dec5560d5191/appointmentData',obj)
    .then((response) => {
     showUsers(response.data)
-    // console.log(response)
+    // console.log(response.id)
    })
    .catch((err) => {
     document.body.innerHTML = document.body.innerHTML + "<h4> Something went wrong </h4>";
     console.log(err)
 });
 }
+window.addEventListener('DOMContentLoaded', () => {
+    axios.get('https://crudcrud.com/api/cfabb4d7a35548378df0dec5560d5191/appointmentData')
+    .then((response) => {
+       for(var i=0; i<response.data.length; i++){
+        showUsers(response.data[i])
+       }
+       })
+       .catch((err) => {
+        console.log(err)
+    });
+})
+
+
+function deleteUser(userId) {
+    axios
+      .delete(`https://crudcrud.com/api/cfabb4d7a35548378df0dec5560d5191/appointmentData/${userId}`)
+      .then((response) => {
+        // Handle successful deletion (e.g., show a message)
+        console.log(`User with ID ${userId} deleted successfully.`);
+      })
+      .catch((err) => {
+        // Handle errors, such as network issues or failed deletions
+        console.log(`Error deleting user with ID ${userId}:`, err);
+      });
+  }
+
+//   function editUser() {
+//     axios.
+//   }
+  
+
 function showUsers(obj){
     let parentElem=document.getElementById('users');
     let childelem =document.createElement('li');
@@ -26,21 +57,21 @@ function showUsers(obj){
     let deletebutton=document.createElement('input');
     deletebutton.type='button';
     deletebutton.value='delete';
+    deletebutton.style.marginLeft = '80px'
     deletebutton.onclick=()=>{
-        parentElem.removeChild(childelem)
+        deleteUser(obj._id);
+        parentElem.removeChild(childelem);
     }
     let editbutton=document.createElement('input')
     editbutton.type='button'
     editbutton.value='Edit'
+    editbutton.style.marginLeft = '5px'
     editbutton.onclick=()=>{
         parentElem.removeChild(childelem)
         document.getElementById('name').value=obj.name
         document.getElementById('email').value=obj.email
 
     }
-
-
-
     childelem.appendChild(deletebutton)
     childelem.appendChild(editbutton)
     parentElem.appendChild(childelem)
